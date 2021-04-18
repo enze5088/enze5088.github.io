b8b0ce549aa7b54e0e09a92cf40488679172ada4
苗圃 <mp5088643@qq.com>
2021-04-19 01:42:41 +0800

    Update deploy-gh.sh

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:mp5088643/mp5088643.github.io.git master
+git push -f git@github.com:mp5088643/mp5088643.github.io.git master:gh-pages
 
 cd -
