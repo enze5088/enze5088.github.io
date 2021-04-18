050fceef4214d7346c32b82dc7c4701a32500dea
苗圃 <mp5088643@qq.com>
2021-04-19 01:01:04 +0800

    commit 14

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,8 @@ cd docs/.vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+githubUrl=git@github.com:mp5088643/mp5088643.github.io.git
+
 git init
 git add -A
 git commit -m 'deploy'
@@ -20,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mp5088643/mp5088643.github.io.git master:gh-pages
+git push -f githubUrl master:gh-pages
 
 cd -
